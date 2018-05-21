693588b87a3ca71a573efe472e54071a63a717f3
Chatchai <chatchai4566@gmail.com>
2018-05-21 22:47:06 +0700

    inital file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testdotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
