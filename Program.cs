d74d5aa6f413a8802c9e8455b084d3b04e3962d8
Chatchai <chatchai4566@gmail.com>
2018-05-21 23:05:47 +0700

    แก้ไขข้อความ

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace testdotnetcore
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! - Essential Docker");
         }
     }
 }
